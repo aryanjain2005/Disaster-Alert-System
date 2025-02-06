526f936a7b8406723eefe67042a8339bdc54ca06
Aryan <aryanjain2462005@gmail.com>
2025-02-06 14:51:59 +0530

    backend url changed

--- frontend/src/utils/api.tsx
+++ frontend/src/utils/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://127.0.0.1:8079";
+const baseUrl = "https://smart-parking-gold.vercel.app/api/proxy";
 //http://127.0.0.1:8079
 //http://14.139.34.101:8079
 //"http://10.8.1.10:8079"
